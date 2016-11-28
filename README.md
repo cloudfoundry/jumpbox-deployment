42c4d0269d79f612615f238463c8973cd8b733a1
dmitriy kalinin <dmitriy@pivotallabs.com>
2016-11-28 03:03:42 -0800

    init

---
+++ README.md
@@ -0,0 +1,53 @@
+# jumpbox-deployment
+
+Deploy single vanilla jumpbox machine. 
+
+IMPORTANT: Make sure to configure security group to allow only necessary traffic! Better yet drop all incoming traffic when jumpbox is not being used.
+
+## Planned
+
+- Apply iptables rule to block all incoming traffic
+  - in addition to relying on IaaS security groups configuration
+- Stop all software aside from SSH after deploy is finished
+- Add `--vars-store /dev/null` CLI support?
+
+## Example on AWS
+
+Requires new [BOSH CLI v0.0.121+](https://github.com/cloudfoundry/bosh-cli).
+
+```
+$ cd ~/workspace
+
+$ git clone https://github.com/cppforlife/jumpbox-deployment
+
+$ mkdir jumpbox-1 && cd jumpbox-1
+
+# Deploy a jumpbox -- ./creds.yml is generated automatically
+$ bosh create-env ~/workspace/jumpbox-deployment/jumpbox.yml \
+  --state ./state.json \
+  --ops-file ~/workspace/jumpbox-deployment/aws/cpi.yml \
+  --vars-store ./creds.yml \
+  -v access_key_id=... \
+  -v secret_access_key=... \
+  -v region=us-east-1 \
+  -v az=us-east-1b \
+  -v default_key_name=jumpbox \
+  -v default_security_groups=[jumpbox] \
+  -v subnet_id=subnet-... \
+  -v internal_cidr=10.0.0.0/24 \
+  -v internal_gw=10.0.0.1 \
+  -v internal_ip=10.0.0.5 \
+  -v external_ip=... \
+  --var-file private_key=...
+
+# Currently, none of the generated credentials are necessary to persist
+$ rm ./creds.yml
+```
+
+Above command requires only two ports open:
+
+```
+Type            Protocol Port Range  Source          Purpose
+SSH             TCP      22          <BOSH CLI's IP> SSH for bootstrapping & final access
+Custom TCP Rule TCP      6868        <BOSH CLI's IP> Agent for bootstrapping
+```
---
+++ test.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+set -eu
+
+vars_store_prefix=/tmp/jumpbox-deployment-test
+
+clean_tmp() {
+  rm -f ${vars_store_prefix}.*
+}
+
+trap clean_tmp EXIT
+
+echo "- AWS"
+bosh interpolate jumpbox.yml --var-errs \
+  -o aws/cpi.yml \
+  --vars-store $(mktemp ${vars_store_prefix}.XXXXXX) \
+  -v internal_cidr=test \
+  -v internal_gw=test \
+  -v internal_ip=test \
+  -v external_ip=test \
+  -v access_key_id=test \
+  -v secret_access_key=test \
+  -v az=test \
+  -v region=test \
+  -v default_key_name=test \
+  -v default_security_groups=[test] \
+  -v private_key=test \
+  -v subnet_id=test \
+  > /dev/null
+
+echo "- GCP"
+bosh interpolate jumpbox.yml --var-errs \
+  -o gcp/cpi.yml \
+  --vars-store $(mktemp ${vars_store_prefix}.XXXXXX) \
+  -v internal_cidr=test \
+  -v internal_gw=test \
+  -v internal_ip=test \
+  -v external_ip=test \
+  -v service_account=test \
+  -v gcp_credentials_json=test \
+  -v project_id=test \
+  -v zone=test \
+  -v network=test \
+  -v subnetwork=test \
+  > /dev/null
+
+echo "- Openstack"
+bosh interpolate jumpbox.yml --var-errs \
+  -o openstack/cpi.yml \
+  --vars-store $(mktemp ${vars_store_prefix}.XXXXXX) \
+  -v internal_cidr=test \
+  -v internal_gw=test \
+  -v internal_ip=test \
+  -v external_ip=test \
+  -v auth_url=test \
+  -v az=test \
+  -v default_key_name=test \
+  -v default_security_groups=test \
+  -v net_id=test \
+  -v openstack_password=test \
+  -v openstack_username=test \
+  -v private_key=test \
+  -v region=test \
+  -v tenant=test \
+  > /dev/null
+
+echo "- vSphere"
+bosh interpolate jumpbox.yml --var-errs \
+  -o vsphere/cpi.yml \
+  -o no-external-ip.yml \
+  --vars-store $(mktemp ${vars_store_prefix}.XXXXXX) \
+  -v internal_cidr=test \
+  -v internal_gw=test \
+  -v internal_ip=test \
+  -v network_name=test \
+  -v vcenter_dc=test \
+  -v vcenter_ds=test \
+  -v vcenter_ip=test \
+  -v vcenter_user=test \
+  -v vcenter_password=test \
+  -v vcenter_templates=test \
+  -v vcenter_vms=test \
+  -v vcenter_disks=test \
+  -v vcenter_cluster=test \
+  > /dev/null
