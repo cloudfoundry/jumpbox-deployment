24e875a6201a54ae5e659924052f49dc1dd61401
dmitriy kalinin <dkalinin@pivotal.io>
2017-04-11 14:36:19 -0700

    clean up gcp vars

--- test.sh
+++ test.sh
@@ -10,8 +10,14 @@ clean_tmp() {
 
 trap clean_tmp EXIT
 
+# Only used for tests below. Ignore it.
+function bosh() {
+  shift 1
+  command bosh int --var-errs --var-errs-unused ${@//--state=*/} > /dev/null
+}
+
 echo "- AWS"
-bosh interpolate jumpbox.yml --var-errs \
+bosh create-env jumpbox.yml \
   -o aws/cpi.yml \
   --vars-store $(mktemp ${vars_store_prefix}.XXXXXX) \
   -v internal_cidr=test \
@@ -29,14 +35,13 @@ bosh interpolate jumpbox.yml --var-errs \
   > /dev/null
 
 echo "- GCP"
-bosh interpolate jumpbox.yml --var-errs \
+bosh create-env jumpbox.yml \
   -o gcp/cpi.yml \
   --vars-store $(mktemp ${vars_store_prefix}.XXXXXX) \
   -v internal_cidr=test \
   -v internal_gw=test \
   -v internal_ip=test \
   -v external_ip=test \
-  -v service_account=test \
   -v gcp_credentials_json=test \
   -v project_id=test \
   -v zone=test \
@@ -46,7 +51,7 @@ bosh interpolate jumpbox.yml --var-errs \
   > /dev/null
 
 echo "- Openstack"
-bosh interpolate jumpbox.yml --var-errs \
+bosh create-env jumpbox.yml \
   -o openstack/cpi.yml \
   --vars-store $(mktemp ${vars_store_prefix}.XXXXXX) \
   -v internal_cidr=test \
@@ -66,7 +71,7 @@ bosh interpolate jumpbox.yml --var-errs \
   > /dev/null
 
 echo "- vSphere"
-bosh interpolate jumpbox.yml --var-errs \
+bosh create-env jumpbox.yml \
   -o vsphere/cpi.yml \
   -o no-external-ip.yml \
   --vars-store $(mktemp ${vars_store_prefix}.XXXXXX) \
