463fd05af2c0b292748265d28ef650689efcea23
Chris Dutra <cdutra@pivotal.io>
2018-11-08 14:46:49 -0800

    Make single commit for bumping stemcells
    [#161689609](https://www.pivotaltracker.com/story/show/161689609)
    
    Co-authored-by: Christopher Hunter <hunter.christopher@icloud.com>

--- ci/tasks/bump-stemcell.sh
+++ ci/tasks/bump-stemcell.sh
@@ -1,12 +1,22 @@
 #!/bin/bash -exu
 
-main() {
-  local STEMCELL_URL="$(cat stemcell/url)"
-  local STEMCELL_SHA="$(cat stemcell/sha1)"
-  local STEMCELL_VERSION="$(cat stemcell/version)"
+create_stemcell_names() {
+  echo bosh-aws-xen-hvm-ubuntu-xenial-go_agent > stemcell-aws/name
+  echo bosh-azure-hyperv-ubuntu-xenial-go_agent > stemcell-azure/name
+  echo bosh-google-kvm-ubuntu-xenial-go_agent > stemcell-gcp/name
+  echo bosh-openstack-kvm-ubuntu-xenial-go_agent > stemcell-openstack/name
+  echo bosh-vsphere-esxi-ubuntu-xenial-go_agent > stemcell-vsphere/name
+}
 
-  pushd jumpbox-deployment
-    cat > /tmp/bump-stemcell-ops.yml <<EOF
+bump_stemcell() {
+  local IAAS=${1?"IaaS is required (e.g. aws, azure, gcp, openstack, vsphere)"}
+
+  local STEMCELL_NAME="$(cat stemcell-${IAAS}/name)"
+  local STEMCELL_VERSION="$(cat stemcell-${IAAS}/version)"
+  local STEMCELL_URL="https://bosh.io/d/stemcells/$STEMCELL_NAME?v=$STEMCELL_VERSION"
+  local STEMCELL_SHA="$(cat stemcell-${IAAS}/sha1)"
+
+  cat > /tmp/bump-stemcell-ops.yml <<EOF
 ---
 - type: replace
   path: /path=~1resource_pools~1name=vms~1stemcell??/value
@@ -15,16 +25,28 @@ main() {
     sha1: ${STEMCELL_SHA}
 EOF
 
-    bosh interpolate -o /tmp/bump-stemcell-ops.yml \
-      ${IAAS}/cpi.yml > /tmp/cpi.yml
+  bosh interpolate -o /tmp/bump-stemcell-ops.yml \
+    jumpbox-deployment/${IAAS}/cpi.yml > /tmp/cpi.yml
 
-    cp /tmp/cpi.yml ${IAAS}/cpi.yml
+  cp /tmp/cpi.yml jumpbox-deployment/${IAAS}/cpi.yml
+}
 
+main() {
+  local STEMCELL_VERSION="$(cat stemcell-aws/version)"
+  local iaas_list=(aws azure gcp openstack vsphere)
+
+  create_stemcell_names
+
+  for iaas in "${iaas_list[@]}"; do
+    bump_stemcell $iaas
+  done
+
+  pushd jumpbox-deployment
     git config user.email "cf-infrastructure@pivotal.io"
     git config user.name "cf-infra-bot"
 
-    git add ${IAAS}/cpi.yml
-    git commit -m "Bump ${IAAS} Stemcell to ${STEMCELL_VERSION}"
+    git add .
+    git commit -m "Bump stemcells to ${STEMCELL_VERSION}"
   popd
 
   cp -R jumpbox-deployment/. updated-jumpbox-deployment
